c6a70e908d52a97750adbc245c7a6b420013f8fe
Christian Poplawski <christianpoplawski@gmail.com>
2019-09-25 14:53:02 +0200

    Split todos in finished and open

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,26 @@
 <template>
-  <ul>
-    <li v-for="(todo, index) in todos" v-bind:key="index">{{ todo.title }}</li>
-  </ul>
+  <div>
+    <h3>Still todo</h3>
+    <ul>
+      <li v-for="(todo, index) in openTodos" v-bind:key="index">{{ todo.title }}</li>
+    </ul>
+    <h3>Completed</h3>
+    <ul>
+      <li v-for="(todo, index) in finishedTodos" v-bind:key="index">{{ todo.title }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
   export default {
-    props: ['todos']
+    props: ['todos'],
+    computed: {
+      openTodos() {
+        return this.todos.filter(todo => !todo.done)
+      },
+      finishedTodos() {
+        return this.todos.filter(todo => todo.done)
+      }
+    }
   }
 </script>
