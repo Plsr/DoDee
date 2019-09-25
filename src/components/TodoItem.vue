18caff030bb92d409173ac71065b6fe33c26746c
Christian Poplawski <christianpoplawski@gmail.com>
2019-09-25 15:51:37 +0200

    Better interactions for todo items

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <button v-on:click="handleClick" class="todo-button" />
+    <button
+      v-on:click="handleClick"
+      class="todo-button"
+      v-bind:class="{ 'todo-button-completed': this.isCompleted}"
+    />
     <span v-bind:class="{ 'todo-text-completed': this.isCompleted }"><slot></slot></span>
   </div>
 </template>
@@ -18,11 +22,34 @@
 
 <style>
   .todo-button {
+    position: relative;
     border: 2px solid #333333;
     border-radius: 4px;
     width: 20px;
     height: 20px;
     margin-right: 1rem;
+  }
+
+  .todo-button:hover:after {
+    content: '✔';
+    position: absolute;
+    font-size: 0.9rem;
+    top: 0;
+    left: 3px;
+  }
+
+  .todo-button-completed {
+    position: relative;
+    border-color: #999999;
+  }
+
+  .todo-button-completed::after {
+    content: '✔';
+    color: #999999;
+    position: absolute;
+    font-size: 0.9rem;
+    top: 0;
+    left: 3px;
   }
 
   .todo-text-completed {
