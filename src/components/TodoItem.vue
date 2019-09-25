9e37beb4c70e982976f9b4d57db6875d74b2356e
Christian Poplawski <christianpoplawski@gmail.com>
2019-09-25 15:35:01 +0200

    Make todos completable

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button v-on:click="handleClick" class="todo-button" />
+    <span v-bind:class="{ 'todo-text-completed': this.isCompleted }"><slot></slot></span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['isCompleted'],
+    methods: {
+      handleClick() {
+        this.$emit('checkbox-click')
+      }
+    }
+  }
+</script>
+
+<style>
+  .todo-button {
+    border: 2px solid #333333;
+    border-radius: 4px;
+    width: 20px;
+    height: 20px;
+    margin-right: 1rem;
+  }
+
+  .todo-text-completed {
+    text-decoration: line-through;
+  }
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,24 +2,49 @@
   <div>
     <h3>Still todo</h3>
     <ul>
-      <li v-for="(todo, index) in openTodos" v-bind:key="index">{{ todo.title }}</li>
+      <todo-item
+        v-for="(todo, index) in openTodos"
+        v-bind:key="index"
+        v-bind:is-completed="todo.done"
+        v-on:checkbox-click="handleTodoClick(todo)"
+      >
+        {{ todo.title }}
+      </todo-item>
     </ul>
     <h3>Completed</h3>
     <ul>
-      <li v-for="(todo, index) in finishedTodos" v-bind:key="index">{{ todo.title }}</li>
+      <todo-item
+        v-for="(todo, index) in finishedTodos"
+        v-bind:key="index"
+        v-bind:is-completed="todo.done"
+        v-on:checkbox-click="handleTodoClick(todo)"
+      >
+        {{ todo.title }}
+      </todo-item>
     </ul>
   </div>
 </template>
 
 <script>
+  import TodoItem from './TodoItem.vue'
+
   export default {
     props: ['todos'],
+    components: {
+      TodoItem
+    },
     computed: {
       openTodos() {
         return this.todos.filter(todo => !todo.done)
       },
       finishedTodos() {
         return this.todos.filter(todo => todo.done)
+      }
+    },
+    methods: {
+      handleTodoClick(todo) {
+        const index = this.todos.indexOf(todo)
+        this.todos[index].done = !this.todos[index].done
       }
     }
   }
